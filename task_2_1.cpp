d838b26a83a1ad23970f289f9a573a8fd4a9c227
Alice Sitedge <a.seledkina@mail.ru>
2019-11-16 10:32:31 +0300

    task 2 done

--- task_2_1.cpp
+++ task_2_1.cpp
@@ -7,41 +7,76 @@
 
 #include <iostream>
 #include <stack>
+#include <queue>
 
 template<class T, class Comparator=std::less<T>>
 class BinaryTree {
     struct Node {
         explicit Node(const T &value) : value(value), left(nullptr), right(nullptr) {}
+
         T value;
         Node *left;
         Node *right;
     };
+
 public:
     BinaryTree() : root(nullptr) {}
-    ~BinaryTree() {}
 
-    bool add(const T &value) {
-        Node *current_node = root;
+    ~BinaryTree() {
+        std::queue<Node *> queue;
 
-        while (true) {
-            if (current_node == nullptr) {
-                current_node = new Node(value);
-                return true;
+        if (root != nullptr) {
+            queue.push(root);
+        }
+
+        while (!queue.empty()) {
+            Node *node = queue.front();
+            queue.pop();
+
+            if (node->left != nullptr) {
+                queue.push(node->left);
+                node->left = nullptr;
+            }
+            if (node->right != nullptr) {
+                queue.push(node->right);
+                node->right = nullptr;
             }
 
+            if (node->left == nullptr && node->right == nullptr) {
+                delete node;
+            } else {
+                queue.push(node);
+            }
+        }
+    }
+
+    bool add(const T &value) {
+        if (root == nullptr) {
+            root = new Node(value);
+        }
+
+        Node *current_node = root;
+        while (true) {
             if (current_node->value == value) {
                 return false;
             }
 
             if (cmp(current_node->value, value)) {
+                if (current_node->right == nullptr) {
+                    current_node->right = new Node(value);
+                }
                 current_node = current_node->right;
             } else {
+                if (current_node->left == nullptr) {
+                    current_node->left = new Node(value);
+                }
                 current_node = current_node->left;
             }
         }
     }
+
     void print() const {
-        std::stack<Node> stack;
+        std::stack<Node *> stack;
 
         if (root == nullptr) {
             std::cout << std::endl;
@@ -49,22 +84,41 @@ public:
         }
 
         Node *current_node = root;
-        while (current_node != nullptr) {
-            while (current_node->left != nullptr) {
-                stack.top(current_node->left);
+        while (true) {
+            while (current_node != nullptr) {
+                stack.push(current_node);
                 current_node = current_node->left;
             }
 
             if (!stack.empty()) {
-                current_node = stack.pop();
+                current_node = stack.top();
+                stack.pop();
                 std::cout << current_node->value << " ";
                 current_node = current_node->right;
+            } else {
+                break;
             }
         }
 
         std::cout << std::endl;
     }
+
 private:
     Node *root;
     Comparator cmp = Comparator();
 };
+
+int main() {
+    BinaryTree<int> tree;
+
+    int size = 0;
+    std::cin >> size;
+    for (int i = 0; i < size; i++) {
+        int elem = 0;
+        std::cin >> elem;
+        tree.add(elem);
+    }
+
+    tree.print();
+    return 0;
+}
